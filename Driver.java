5f98bc7b274e5516381423f7685b8804c0eb028f
Jerry He <jerryhe1305@gmail.com>
2024-06-01 22:56:39 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,58 @@
+import java.util.Arrays;
+import java.io.File;
+import java.util.Scanner;
+import java.io.IOException;
+
 public class Driver {
-	public static void main(String [] args) {
+	
+	public static void main(String [] args) throws IOException {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-	double [] c1 = {6,0,0,5};
-	Polynomial p1 = new Polynomial(c1);
-	double [] c2 = {0,-2,0,0,-9};
-	Polynomial p2 = new Polynomial(c2);
-	Polynomial s = p1.add(p2);
-	System.out.println("s(0.1) = " + s.evaluate(0.1));
-	if(s.hasRoot(1))
-		System.out.println("1 is a root of s");
-	else
-		System.out.println("1 is not a root of s");
+		
+		int[] arr1_deg = {4, 5, 3};
+		double[] arr1_coeff = {3, 6, 2};
+		Polynomial p1 = new Polynomial(arr1_coeff, arr1_deg);
+		
+		int[] arr2_deg = {0, 1, 2, 3};
+		double[] arr2_coeff = {1, 2, 3, 4};
+		Polynomial p2 = new Polynomial(arr2_coeff, arr2_deg);
+		
+		Polynomial sum1 = p1.add(p2);
+		System.out.println("sum1(1) = " + sum1.evaluate(1));
+		
+		Polynomial prod1 = p1.multiply(p2);
+		prod1.print_poly();
+		
+		// print polynomial
+		//sum1.print_poly();
+		// check empty array
+		Polynomial empty = new Polynomial();
+		//System.out.println(Arrays.toString(empty.poly_coeff));
+		
+		//test multiply
+		int[] arr3_deg = {1, 0};
+		double[] arr3_coeff = {1, 1};
+		Polynomial p3 = new Polynomial(arr3_coeff, arr3_deg);
+		
+		int[] arr4_deg = {0, 1};
+		double[] arr4_coeff = {1, 1};
+		Polynomial p4 = new Polynomial(arr4_coeff, arr4_deg);
+		
+		Polynomial result = p3.multiply(p4);
+		//result.print_poly();
+
+		// Test file, uncomment to use
+		/*
+		File testFile = new File("test.txt");
+		Polynomial file_poly = new Polynomial(testFile);
+		file_poly.print_poly();
+		
+		System.out.println(Arrays.toString(file_poly.poly_coeff));
+		System.out.println(Arrays.toString(file_poly.poly_deg));
+		
+		// Test write
+		
+		file_poly.saveToFile("remake.txt");
+		*/
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,64 +1,219 @@
-public class Polynomial {
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
-		double[] poly;
+public class Polynomial {
+	int[] poly_deg;
+	double[] poly_coeff;
+	
+	public Polynomial() {
+		poly_deg = new int[0];
+		poly_coeff = new double[0];
 		
-		public Polynomial() {
-			poly = new double[1];
-			poly[0] = 0;
-			
-		}
-		public Polynomial(double arr[]) {
-			poly = arr;
+	}
+	public Polynomial(double[] coeff, int[] deg) {
+		poly_coeff = coeff;
+		poly_deg = deg;
+	}
+	
+	public Polynomial(File file_name) throws FileNotFoundException {
+		Scanner reader = new Scanner(file_name);
+		String poly_line = reader.nextLine();
+		
+		// split at the signs, using positive lookahead to keep signs	
+		String[] new_poly = poly_line.split("((?=[-+]))");
+		int counter = 0;
+		poly_deg = new int[new_poly.length];
+		poly_coeff = new double[new_poly.length];
+		for (int i = 0; i < new_poly.length; i++) {
+			String[] temp = new_poly[i].split("[xX]");
+			// no x value, only coefficient term
+			if (temp.length == 1) {
+				poly_deg[counter] = 0;
+				poly_coeff[counter] = Double.parseDouble(temp[0]);
+			}
+			else {
+				poly_deg[counter] = Integer.parseInt(temp[1]);
+				poly_coeff[counter] = Double.parseDouble(temp[0]);
+			}
+			counter += 1;	
 		}
 		
-		public Polynomial add(Polynomial arr) {
-			int large = Math.max(poly.length, arr.poly.length);
-			double[] sum = new double[large];
-			
-			int small = Math.min(poly.length, arr.poly.length);
-			
-			int counter = 0;
-			for (int i = 0; i < small; i++) {
-				sum[i] = poly[i] + arr.poly[i];
-				counter++;
+		reader.close();
+	}
+	
+	// used to calculate the largest degree
+	public double max_deg(double[] arr) {
+		double max = 0;
+		for (int i = 0; i < arr.length; i++) {
+			double curr = arr[i];
+			if (curr > max) 
+				max = curr;
+		}
+		return max;
+	}
+	
+	// checks if an element exists in an array, returns the array index if yes
+	// otherwise returns -1
+	private int contains(double[] arr, double element) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == element) 
+				return i;
+		}
+		
+		return -1;
+	}
+	// overload method for type integer array
+	private int contains(int[] arr, double element) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == element) 
+				return i;
+		}
+		
+		return -1;
+	}
+	
+	
+	// used to find new length of sum array
+	private int unique_degs(int[] arr1, int[] arr2) {
+		int length = 0;
+		int arr1_size = arr1.length;
+		int arr2_size = arr2.length;
+		// upper bound number on unique degrees is the sum
+		double[] checked = new double[arr1_size + arr2_size];
+		
+		// since checked array gets initialized to 0
+		if (contains(arr1, 0) != -1 || contains(arr2, 0) != -1)
+			length += 1;
+		
+		// add unique elements from arr1
+		for (int i = 0; i < arr1_size; i++) {
+			// if the current element is unique
+			if (contains(checked, arr1[i]) == -1){
+				checked[i] = arr1[i];
+				length += 1;
 			}
-			
-			// if the calling object is smaller
-			if (poly.length == small) {
-				// add the rest of the argument polynomial to the sum polynomial as is
-				while (counter < arr.poly.length) {
-					sum[counter] = arr.poly[counter];
-					counter++;
-				}
+		}
+		// add unique elements from arr2
+		for (int i = arr1_size; i < arr1_size + arr2_size; i++) {
+			// if the current element is unique
+			if (contains(checked, arr2[i - arr1_size]) == -1){
+				checked[i] = arr2[i - arr1_size];
+				length += 1;
 			}
+		}
+		
+		return length;
+	}
+	
+	public Polynomial add(Polynomial arr) {
+		
+		int size = unique_degs(this.poly_deg, arr.poly_deg);
+		
+		double[] sum_coeff = new double[size];
+		int[] sum_deg = new int[size];
+		
+		// initialize sum_deg to have -1 as values instead of 0
+		for (int i = 0; i < sum_deg.length; i++) {
+			sum_deg[i] = -1;
+		}
+		
+		int new_arr_count = 0;
+		
+		// assuming at this point there are no redundant exponents
+		for (int i = 0; i < this.poly_deg.length; i++) {
+			sum_deg[new_arr_count] = this.poly_deg[i];
+			// if the argument polynomial has a term with the same degree
+			int arr_match = contains(arr.poly_deg, this.poly_deg[i]);
+			if (arr_match != -1) 		
+				sum_coeff[new_arr_count] = this.poly_coeff[i] + arr.poly_coeff[arr_match];
 			
 			else {
-				while (counter < poly.length) {
-					sum[counter] = poly[counter];
-					counter++;
-				}
+				sum_coeff[new_arr_count] = this.poly_coeff[i];
 			}
 			
-			Polynomial new_poly = new Polynomial(sum);
-			return new_poly;
+			// increment the new array index
+			new_arr_count += 1;
 		}
+		// check if the argument array has terms not yet added
 		
-		public double evaluate(double value) {
-			double result = 0;
-			
-			for (int i = 0; i < poly.length; i++) {
-				// sum the passed in x-value to the current power multiplied by the coefficient
-				result += poly[i] * Math.pow(value, i);
+		for (int i = 0; i < arr.poly_deg.length; i++) {
+			// if the current degree is not in the new polynomial degree array
+			if (contains(sum_deg, arr.poly_deg[i]) == -1) {
+				sum_deg[new_arr_count] = arr.poly_deg[i];
+				sum_coeff[new_arr_count] = arr.poly_coeff[i];
+				new_arr_count += 1;
 			}
-			
-			return result;
 		}
 		
-		public boolean hasRoot(double value) {
-			if (evaluate(value) == 0) {
-				return true;
+		Polynomial new_poly = new Polynomial(sum_coeff, sum_deg);
+		return new_poly;
+	}
+	
+	public double evaluate(double value) {
+		double result = 0;
+		
+		for (int i = 0; i < poly_coeff.length; i++) {
+			// sum the passed in x-value to the current power multiplied by the coefficient
+			result += poly_coeff[i] * Math.pow(value, poly_deg[i]);
+		}
+		
+		return result;
+	}
+	
+	public boolean hasRoot(double value) {
+		if (evaluate(value) == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public Polynomial multiply(Polynomial poly) {
+		Polynomial result = new Polynomial();
+		for (int i = 0; i < this.poly_deg.length; i++) {
+			for (int j = 0; j < poly.poly_deg.length; j++) {
+				double[] term_coeff = {this.poly_coeff[i] * poly.poly_coeff[j]};
+				int[] term_deg = {this.poly_deg[i] + poly.poly_deg[j]};
+	
+				Polynomial temp = new Polynomial(term_coeff, term_deg);
+				result = result.add(temp);
 			}
-			return false;
 		}
 		
+		return result;
+	}
+	
+	// save to file method
+	public void saveToFile(String name) throws IOException {
+		String result = "";
+		for (int i = 0; i < poly_deg.length; i++) {
+
+			// include + sign if not at start of string
+			if (i > 0 && poly_coeff[i] > 0)
+
+				result = result.concat("+");
+				
+			result = result.concat(Double.toString(poly_coeff[i]));
+			
+			if (poly_deg[i] > 0) {
+
+				result = result.concat("x");
+				result = result.concat(Integer.toString(poly_deg[i]));
+			}	
+		}
+
+		FileWriter output = new FileWriter(name, false);
+		output.write(result);
+		output.close();
+	}
+	
+	// print polynomial
+	public void print_poly() {
+		for (int i = 0; i < this.poly_deg.length; i++) {
+			System.out.print(this.poly_coeff[i] + "x^" + this.poly_deg[i] + " ");
+		}
+		System.out.println();
+	}
 }
